fa59f7fac4b7d0a716d331e4f2bb17e3ccdfa400
PapaKarlo787 <33232815+PapaKarlo787@users.noreply.github.com>
2025-02-07 17:52:18 +0500

    parallelism started

--- GAME/converter/converter.cpp
+++ GAME/converter/converter.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
-#include <set>
+#include <thread>
+#include <mutex>
 #include <stdint.h>
 #include <functional>
 #include <numeric>
@@ -12,22 +13,23 @@ uint32_t err;
 #define PALETE_SIZE 256
 
 using namespace std;
+mutex mtx;
 
-void writeRepreses(int fd, vector<RGB>& represes) {
+void writeCodeBook(int fd, vector<RGB>& codeBook) {
 	char data[256 * 3];
 	for (int i = 0; i < PALETE_SIZE; i++) {
-		data[i * 3 + 0] = represes[i].r;
-		data[i * 3 + 1] = represes[i].g;
-		data[i * 3 + 2] = represes[i].b;
+		data[i * 3 + 0] = codeBook[i].r;
+		data[i * 3 + 1] = codeBook[i].g;
+		data[i * 3 + 2] = codeBook[i].b;
 	}
 	err += write(fd, data, sizeof(data));
 }
 
-inline int findNearestRepresentor(RGB const& color, vector<RGB> const& representors) {
+inline int findNearestElem(RGB const& color, vector<RGB> const& codeBook) {
 	int result = 0;
 	uint32_t dist = -1;
 	for (int i = 0; i < PALETE_SIZE; i++) {
-		uint32_t d = color.dist(representors[i]);
+		uint32_t d = color.dist(codeBook[i]);
 		if (dist > d) {
 			dist = d;
 			result = i;
@@ -36,50 +38,65 @@ inline int findNearestRepresentor(RGB const& color, vector<RGB> const& represent
 	return result;
 }
 
-void gla(vector<RGB>& represes, vector<RGB> const& colors) {
-	RGB newRepreses[PALETE_SIZE];
+void gla(vector<RGB>& codeBook, vector<RGB> const& colors) {
+	RGB newCodeBook[PALETE_SIZE];
 	int cntMembers[PALETE_SIZE];
 	for (int i = 0; i < PALETE_SIZE; i++) {
-		newRepreses[i] = RGB();
+		newCodeBook[i] = RGB();
 		cntMembers[i] = 0;
 	}
 	for (int i = 0; i < GLA_STEPS; i++) {
 		for (auto& color : colors) {
-			int i = findNearestRepresentor(color, represes);
-			newRepreses[i] += color;
+			int i = findNearestElem(color, codeBook);
+			newCodeBook[i] += color;
 			cntMembers[i]++;
 		}
 		for (int i = 0; i < PALETE_SIZE; i++) {
-			represes[i] = cntMembers[i] ? newRepreses[i] / cntMembers[i] : RGB();
-			newRepreses[i] = RGB();
+			codeBook[i] = cntMembers[i] ? newCodeBook[i] / cntMembers[i] : RGB();
+			newCodeBook[i] = RGB();
 			cntMembers[i] = 0;
 		}
 	}
 }
 
-void medianCut(set<vector<RGB>*>& colorSets) {
-	function<bool(RGB const&, RGB const&)> r = [](RGB const& a, RGB const& b) {return a.r < b.r; };
-	function<bool(RGB const&, RGB const&)> g = [](RGB const& a, RGB const& b) {return a.g < b.g; };
-	function<bool(RGB const&, RGB const&)> b = [](RGB const& a, RGB const& b) {return a.b < b.b; };
-	auto vSizePred = [](vector<RGB>* a, vector<RGB>* b) {return a->size() < b->size(); };
-
-	for (int i = 0; i < PALETE_SIZE - 1; i++) {
-		vector<RGB>* curSet = *max_element(colorSets.begin(), colorSets.end(), vSizePred);
-		colorSets.erase(curSet);
-		uint32_t m = max_element(curSet->begin(), curSet->end(), r) - min_element(curSet->begin(), curSet->end(), r);
+template<typename T>
+RGB medianCut(T& result, T::iterator& begin, T::iterator& end, int floor) {
+	static function<bool(RGB const&, RGB const&)> r = [](RGB const& a, RGB const& b) {return a.r < b.r; };
+	static function<bool(RGB const&, RGB const&)> g = [](RGB const& a, RGB const& b) {return a.g < b.g; };
+	static function<bool(RGB const&, RGB const&)> b = [](RGB const& a, RGB const& b) {return a.b < b.b; };
+	static auto vSizePred = [](vector<RGB>* a, vector<RGB>* b) {return a->size() < b->size(); };
+	vector<thread> treads;
+	
+	for (int i = floor - 1; i >= 0; i--) {
+		uint32_t m = max_element(begin, end, r) - min_element(begin, end, r);
 		function<bool(RGB const&, RGB const&)> comp = r;
 		for (auto f : { g, b }) {
-			auto s = max_element(curSet->begin(), curSet->end(), f) - min_element(curSet->begin(), curSet->end(), f);
+			auto s = max_element(begin, end, f) - min_element(begin, end, f);
 			if (m > s) {
 				m = s;
 				comp = f;
 			}
 		}
-		sort(curSet->begin(), curSet->end(), comp);
-		m = curSet->size() / 2;
-		colorSets.insert(new vector<RGB>(curSet->begin(), curSet->end() - m));
-		colorSets.insert(new vector<RGB>(curSet->begin() + m, curSet->end()));
-		delete curSet;
+		sort(begin, end, comp);
+		int size = end - begin;
+		m = size / 2;
+		if (i) {
+			treads.push_back(thread(mediancut, result, begin + m, end, i));
+			end = begin + m;
+		} else {
+			RGB res1, res2;
+			if (size) {
+				res1 = accumulate(begin, end - m, RGB()) / size;
+				res2 = accumulate(begin + m, end, RGB()) / size;
+			}
+			mtx.lock();
+			result.push_back(res1);
+			result.push_back(res2);
+			mtx.unlock();
+		}
+	}
+	for (int i = 0; i < floor - 1; i++) {
+		threads[i].join();
 	}
 }
 
@@ -94,26 +111,19 @@ int main(int n, const char** args) {
 	}
 
 	getColors(n - 3, args + 3, colors, files);
-	set<vector<RGB>*> colorSets{ new vector<RGB>(colors.begin(), colors.end()) };
-	medianCut(colorSets);
-
-	vector<RGB> represes;
-	for (auto& s : colorSets) {
-		RGB sum = accumulate(s->begin(), s->end(), RGB());
-		represes.push_back(s->size() ? sum / s->size() : RGB());
-		delete s;
-	}
-	gla(represes, colors);
+	vector<RGB> codeBook;
+	medianCut(codeBook, colors.begin(), colors.end(), 8);
+	gla(codeBook, colors);
 
 	for(auto color : colors) {
-		int ind = findNearestRepresentor(color, represes);
+		int ind = findNearestElem(color, codeBook);
 		color.f->addResData(ind, color.pos);
 	}
 
 	int fd = open(args[2], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	uint16_t size = files.size();
 	err += write(fd, &size, sizeof(size));
-	writeRepreses(fd, represes);
+	writeCodeBook(fd, codeBook);
 	for (int i = 0; i < size; i++) {
 		files[i].writef(fd);
 	}
